6ff5108b60117abf94b24781fe5447e468d1c5eb
invi_1998 <2267661826@qq.com>
2023-08-15 16:31:24 +0800

    细化管线绑定-shader和输入布局

--- INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderingPipeline.cpp
+++ INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderingPipeline.cpp
@@ -17,5 +17,22 @@ void FRenderingPipeline::BuildPipeline()
 	DirectXRootSignature.BuildRootSignature();	// 构建根签名
 	DirectXPipelineState.BindRootSignature(DirectXRootSignature.GetRootSignature());	// 绑定根签名
 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///构建shader HLSL
+	VertexShader.BuildShader(L"Shader/HLSL/V_P_Shader.hlsl", "VSMain", "vs_5_0");
+	PixelShader.BuildShader(L"Shader/HLSL/V_P_Shader.hlsl", "PSMain", "ps_5_0");
+
+	// 输入布局
+	InputElementDesc =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},	// 颜色，这里这个偏移是12字节，因为我们上面位置是3个元素，每个元素是4字节（32位），所以到了颜色这里就是 3*4 = 12字节的偏移了
+	};
+
+	DirectXPipelineState.BindInputLayout(InputElementDesc.data(), InputElementDesc.size());
+
+	// 绑定shader
+	DirectXPipelineState.BindShader(VertexShader, PixelShader);
+
 	DirectXPipelineState.BuildPipeline();	// 构建渲染管线
 }
