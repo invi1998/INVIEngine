459ac9f4a1215d91bd64d79ea481b8e2f3fc4dfc
invi_1998 <2267661826@qq.com>
2023-09-08 17:22:23 +0800

    修复常量缓冲区字节对齐bug

--- INVIEngine/Engine/Material/Core/MaterialConstantBuffer.h
+++ INVIEngine/Engine/Material/Core/MaterialConstantBuffer.h
@@ -8,9 +8,13 @@ struct FMaterialConstantBuffer
 {
 	FMaterialConstantBuffer();
 
-	EMaterialType MaterialType;
-	XMFLOAT4 BaseColor;
-	float Roughness; // 材质粗糙度
+	EMaterialType MaterialType;		// 4字节
+	int Reserved1;					// 4字节
+	int Reserved2;					// 4
+	int Reserved3;					// 4
 
-	XMFLOAT4X4 MaterialProjectionMatrix;
+	XMFLOAT4 BaseColor;				// 16字节
+	float Roughness; // 材质粗糙度	// 4字节
+
+	XMFLOAT4X4 MaterialProjectionMatrix;	// 64字节
 };
