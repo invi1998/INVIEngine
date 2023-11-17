a328cb68b494697b0bdd62a3c0a6ade669d3124c
invi_1998 <2267661826@qq.com>
2023-11-17 17:23:15 +0800

    构建立方体贴图资源

--- INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderTarget/CubeMapRenderTarget.cpp
+++ INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderTarget/CubeMapRenderTarget.cpp
@@ -43,4 +43,34 @@ void FCubeMapRenderTarget::ResetScissorRect()
 
 void FCubeMapRenderTarget::BuildRenderTarget()
 {
+	// 资源描述
+	CD3DX12_RESOURCE_DESC CubeMapResourceDesc{};
+
+	memset(&CubeMapResourceDesc, 0, sizeof(CD3DX12_HEAP_PROPERTIES));
+
+	CubeMapResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;		// 资源维度2D纹理
+	CubeMapResourceDesc.Alignment = 0;		// 对齐方式
+	CubeMapResourceDesc.Width = Width;		// 资源宽度
+	CubeMapResourceDesc.Height = Height;	// 资源高度
+	CubeMapResourceDesc.DepthOrArraySize = 6;		// 资源数量（6个，因为我们的是立方体贴图）（如果你的资源是3D维度，那么这个值就是指的资源深度，如果是1D, 2D，那么这个值就是指资源数组的长度）
+	CubeMapResourceDesc.MipLevels = 1;		// mipmap
+	CubeMapResourceDesc.Format = Format;		// 数据格式
+	CubeMapResourceDesc.SampleDesc.Count = 1;	// 采样描述-数量-1
+	CubeMapResourceDesc.SampleDesc.Quality = 0;	// 采样描述-质量-0
+	CubeMapResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;	// 纹理布局方式-未知布局（由驱动自行决定）
+	CubeMapResourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET; // 资源标记-标记资源可以被允许设置为我们的渲染目标（renderTarget）
+
+
+	// 创建默认堆
+	CD3DX12_HEAP_PROPERTIES BufferProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+
+	// 创建提交资源
+	ANALYSIS_RESULT(GetD3dDevice()->CreateCommittedResource(
+		&BufferProperties,
+		D3D12_HEAP_FLAG_NONE,
+		&CubeMapResourceDesc,
+		D3D12_RESOURCE_STATE_COMMON,
+		nullptr,
+		IID_PPV_ARGS(RenderTargetMap.GetAddressOf())
+	));
 }
