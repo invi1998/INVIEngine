5649541131537b0c6b61f15ec5835bd32f887f16
invi_1998 <2267661826@qq.com>
2023-12-25 17:16:18 +0800

    笛卡尔坐标转球面坐标

--- INVIEngine/Engine/Math/EngineMath.h
+++ INVIEngine/Engine/Math/EngineMath.h
@@ -19,4 +19,28 @@ namespace EngineMath
 	{
 		return DirectX::XMFLOAT3(InV3d.x, InV3d.y, InV3d.z);
 	}
+
+	// 知道世界坐标（笛卡尔坐标），求出球坐标
+	DirectX::XMFLOAT3 CartesianToSpherical(const DirectX::XMFLOAT3& InCartesian)
+	{
+		DirectX::XMFLOAT3 OutSpherical{};
+		// 半径R
+		OutSpherical.x = sqrtf(InCartesian.x * InCartesian.x + InCartesian.y * InCartesian.y + InCartesian.z * InCartesian.z);
+		// theta
+		OutSpherical.y = atan2f(InCartesian.y, InCartesian.x);
+		// phi
+		OutSpherical.z = acosf(InCartesian.z / OutSpherical.x);
+		return OutSpherical;
+
+		//float anglePre = 180.f / DirectX::XM_PI;
+
+		//// 先求出半径
+		//float r = sqrtf(InCartesian.x * InCartesian.x + InCartesian.y * InCartesian.y + InCartesian.z * InCartesian.z);
+		//// 求出theta
+		//float theta = acos(InCartesian.y / r) * anglePre;
+		//// 求出phi
+		//float phi = atan2f(InCartesian.z, InCartesian.x) * anglePre;
+
+		//return DirectX::XMFLOAT3{ r, theta, phi };
+	}
 }
