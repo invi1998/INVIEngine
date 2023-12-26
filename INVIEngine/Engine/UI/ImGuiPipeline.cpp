3cad0bfa43afc837ba45ff98bd233a5faa918b07
invi_1998 <2267661826@qq.com>
2023-12-26 14:27:22 +0800

    ImGUI初始化

--- INVIEngine/Engine/UI/ImGuiPipeline.cpp
+++ INVIEngine/Engine/UI/ImGuiPipeline.cpp
@@ -8,6 +8,25 @@ FImGuiPipeline::FImGuiPipeline()
 
 void FImGuiPipeline::Initialize(ID3D12DescriptorHeap* heap, UINT offset)
 {
+	// 检查版本
+	IMGUI_CHECKVERSION();
+
+	// 初始化上下文
+	ImGui::CreateContext();
+
+	// 设置样式
+	ImGui::StyleColorsDark();
+
+	// 初始化Win32平台
+	ImGui_ImplWin32_Init(GetMainWindowsHandle());
+
+	// 获取偏移后的CPU/GPU句柄
+	const auto cpuHandle = CD3DX12_CPU_DESCRIPTOR_HANDLE(heap->GetCPUDescriptorHandleForHeapStart(), offset, GetD3dDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV));
+	const auto gpuHandle = CD3DX12_GPU_DESCRIPTOR_HANDLE(heap->GetGPUDescriptorHandleForHeapStart(), offset, GetD3dDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV));
+
+	// 初始化DX12平台
+	ImGui_ImplDX12_Init(GetD3dDevice().Get(), 1, DXGI_FORMAT_R8G8B8A8_UNORM, heap, cpuHandle, gpuHandle);
+
 }
 
 void FImGuiPipeline::Draw(float deltaTime)
--- INVIEngine/Engine/UI/ImGuiPipeline.h
+++ INVIEngine/Engine/UI/ImGuiPipeline.h
@@ -3,7 +3,8 @@
 #define IMGUI_IMPL_DIRECTX12_RENDERING_HELPERS
 #include "imgui.h"
 #include "Interface/DirectXDeviceInterface.h"
-// #include "imgui_impl_win32.h"
+#include "backends/imgui_impl_win32.h"
+#include "backends/imgui_impl_dx12.h"
 
 struct FImGuiPipeline : public IDirectXDeviceInterface_Struct
 {
