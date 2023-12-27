05668dfefc3c2964a244baed33e0c1b5107435d8
invi_1998 <2267661826@qq.com>
2023-12-27 19:18:38 +0800

    save

--- INVIEngine/Engine/Debug/EngineDebug.h
+++ INVIEngine/Engine/Debug/EngineDebug.h
@@ -8,10 +8,7 @@
 		ENGINE_LOG_ERROR("Error = %i", (int)(HandleResult));\
 		assert(0);\
 	}\
-	else if (SUCCEEDED(HandleResult))\
-	{\
-		ENGINE_LOG_SUCCESS("Success !");\
-	}\
+	else if (SUCCEEDED(HandleResult)){}\
 }
 
 
