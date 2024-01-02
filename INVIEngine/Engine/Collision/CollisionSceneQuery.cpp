2fee9b3f3e81b7d7123691b9a08f9d4f3951fae2
invi_1998 <2267661826@qq.com>
2024-01-02 19:38:00 +0800

    AABB

--- INVIEngine/Engine/Collision/CollisionSceneQuery.cpp
+++ INVIEngine/Engine/Collision/CollisionSceneQuery.cpp
@@ -1,2 +1,26 @@
 #include "EngineMinimal.h"
 #include "CollisionSceneQuery.h"
+
+#include "Rendering/Core/DirectX12/RenderingPipeline/Geometry/GeometryMap.h"
+
+bool FCollisionSceneQuery::RayCastSingleQuery(CWorld* world, const XMFLOAT3& origin, const XMFLOAT3& direction, const XMMATRIX& viewInverseMatrix, EngineType::FHitResult& OutHitResult)
+{
+	// 遍历FGeometry::RenderingDataPoolVector，找到所有的mesh
+	for (auto& renderData : FGeometry::RenderingDataPoolVector)
+	{
+		float time = 0.f;
+		// 射线是否和AABB包围盒相交
+		if (renderData->Bounds.intersects(origin, direction, time))
+		{
+			// 射线是否和模型相交
+			if (renderData->Mesh->Intersect(origin, direction, time, viewInverseMatrix))
+			{
+				OutHitResult.HitObject = renderData->Mesh->GetOwner();
+				OutHitResult.HitTime = time;
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
--- INVIEngine/Engine/Collision/CollisionSceneQuery.h
+++ INVIEngine/Engine/Collision/CollisionSceneQuery.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "EngineType.h"
+
+class CWorld;
 
 // Åö×²²éÑ¯
-class FCollisionSceneQuery
+struct FCollisionSceneQuery
 {
+	// Åö×²²éÑ¯
+	static bool RayCastSingleQuery(CWorld* world, const XMFLOAT3& origin, const XMFLOAT3& direction, const XMMATRIX& viewInverseMatrix, EngineType::FHitResult& OutHitResult);
 };
 
