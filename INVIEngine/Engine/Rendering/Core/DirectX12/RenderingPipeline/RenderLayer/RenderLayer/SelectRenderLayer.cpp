9370ba11d3428aa3d7c34e3ac0974975be1284f6
invi_1998 <2267661826@qq.com>
2024-01-03 20:37:53 +0800

    shader上色

--- INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderLayer/RenderLayer/SelectRenderLayer.cpp
+++ INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderLayer/RenderLayer/SelectRenderLayer.cpp
@@ -22,8 +22,8 @@ void FSelectRenderLayer::BuildShader()
 	std::vector<D3D_SHADER_MACRO> D3dShaderMacro;
 	ShaderType::ToD3DShaderMacro(ShaderMacro, D3dShaderMacro);
 
-	VertexShader.BuildShader(L"Shader/HLSL/V_P_Shader.hlsl", "VSMain", "vs_5_1", D3dShaderMacro.data());
-	PixelShader.BuildShader(L"Shader/HLSL/V_P_Shader.hlsl", "PSMain", "ps_5_1", D3dShaderMacro.data());
+	VertexShader.BuildShader(L"Shader/HLSL/Select.hlsl", "VSMain", "vs_5_1", D3dShaderMacro.data());
+	PixelShader.BuildShader(L"Shader/HLSL/Select.hlsl", "PSMain", "ps_5_1", D3dShaderMacro.data());
 	// 绑定shader
 	DirectXPipelineState->BindShader(VertexShader, PixelShader);
 
@@ -31,10 +31,7 @@ void FSelectRenderLayer::BuildShader()
 	InputElementDesc =
 	{
 		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
-		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},	// 颜色，这里这个偏移是12字节，因为我们上面位置是3个元素，每个元素是4字节（32位），所以到了颜色这里就是 3*4 = 12字节的偏移了
-		{"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 28, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},	 // 法线，这里这个偏移是28字节，因为我们上面位置是3个元素，每个元素是4字节（32位）, 加上颜色4个元素，所以到了法线这里，就得偏移 （3+4）*4 = 28字节的偏移了
-		{"TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 40, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}, // 切线（U切线），这里这个偏移是32字节，因为我们上面位置是3个元素，每个元素是4字节（32位）, 加上颜色4个元素，再加上3个法线元素，到了切线这里，就得偏移 （3+4+3）*4 = 40字节的偏移了
-		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 52, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}	// uv坐标，这里这个偏移是52字节，因为我们上面位置是3个元素，每个元素是4字节（32位）, 加上颜色4个元素，再加上3个法线元素，再加上3个切线元素，到了uv这里，就得偏移 （3+4+3+3）*4 = 52字节的偏移了
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}	// uv坐标，这里这个偏移是52字节，因为我们上面位置是3个元素，每个元素是4字节（32位），到了uv这里，就得偏移 （3）*4 = 12字节的偏移了
 
 	};
 
---
+++ INVIEngine/Shader/HLSL/Select.hlsl
@@ -0,0 +1,45 @@
+// 射线选中物体的shader
+
+#include "Material.hlsl"
+
+struct MeshVertexIn
+{
+	float3 Position : POSITION; // 位置
+	float2 Texcoord : TEXCOORD; // UV
+};
+
+struct MeshVertexOut
+{
+	float4 PositionH : SV_POSITION;
+	float2 Texcoord : TEXCOORD;
+};
+
+MeshVertexOut VSMain(MeshVertexIn mv)
+{
+	MaterialConstBuffer MatConstBuffer = Materials[MaterialID];
+	
+	MeshVertexOut Out = (MeshVertexOut) 0;
+	
+	// 将模型的顶点坐标转为世界坐标
+	float4 PositionWorld = mul(float4(mv.Position, 1.f), WorldMatrix);
+	
+	// 将世界坐标转为视口裁剪空间坐标
+	Out.PositionH = mul(PositionWorld, ViewportProjectionMatrix);
+	
+	// UV
+    // 先将传入的uv坐标和模型的纹理变换相乘，得到纹理变换后的UV
+	float4 TextureTexcoord = mul(float4(mv.Texcoord, 0.f, 1.f), TextureTransformationMatrix);
+    // 然后再与材质的变换矩阵相乘，得到材质变换后的UV，就是我们最终的顶点UV了
+	Out.Texcoord = mul(TextureTexcoord, MatConstBuffer.MaterialProjectionMatrix).xy;
+	
+	return Out;
+
+}
+
+
+float4 PSMain(MeshVertexOut mvOut) : SV_TARGET
+{
+	return float4(0.9f, 0.23f, 0.11f, 1.0f);
+}
+
+
