dea8e83c85d66eea5e516a07d59d3cf23d02b5fe
invi_1998 <2267661826@qq.com>
2024-01-04 14:45:14 +0800

    菲尼尔方式显示被选中的物体

--- INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderLayer/RenderLayer/SelectRenderLayer.cpp
+++ INVIEngine/Engine/Rendering/Core/DirectX12/RenderingPipeline/RenderLayer/RenderLayer/SelectRenderLayer.cpp
@@ -31,7 +31,10 @@ void FSelectRenderLayer::BuildShader()
 	InputElementDesc =
 	{
 		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
-		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}	// uv坐标，这里这个偏移是52字节，因为我们上面位置是3个元素，每个元素是4字节（32位），到了uv这里，就得偏移 （3）*4 = 12字节的偏移了
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},	// 颜色，这里这个偏移是12字节，因为我们上面位置是3个元素，每个元素是4字节（32位），所以到了颜色这里就是 3*4 = 12字节的偏移了
+		{"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 28, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},	 // 法线，这里这个偏移是28字节，因为我们上面位置是3个元素，每个元素是4字节（32位）, 加上颜色4个元素，所以到了法线这里，就得偏移 （3+4）*4 = 28字节的偏移了
+		{"TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 40, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}, // 切线（U切线），这里这个偏移是32字节，因为我们上面位置是3个元素，每个元素是4字节（32位）, 加上颜色4个元素，再加上3个法线元素，到了切线这里，就得偏移 （3+4+3）*4 = 40字节的偏移了
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 52, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}	// uv坐标，这里这个偏移是52字节，因为我们上面位置是3个元素，每个元素是4字节（32位）, 加上颜色4个元素，再加上3个法线元素，再加上3个切线元素，到了uv这里，就得偏移 （3+4+3+3）*4 = 52字节的偏移了
 
 	};
 
@@ -42,6 +45,24 @@ void FSelectRenderLayer::BuildShader()
 void FSelectRenderLayer::BuildPSO()
 {
 	FRenderLayer::BuildPSO();
+
+	D3D12_RENDER_TARGET_BLEND_DESC RenderTargetBlendDesc;
+	RenderTargetBlendDesc.BlendEnable = true;		// 开启图层混合
+	RenderTargetBlendDesc.LogicOpEnable = false;	// 关闭逻辑混合（开启混合必须将该值关闭，因为他两是互斥的）
+
+	RenderTargetBlendDesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;	// 设置源颜色和混合后颜色之间的乘法方式，设置为该值表示 将源颜色乘以源像素的 alpha 值。
+	RenderTargetBlendDesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;	// 表示目标颜色和混合后颜色之间的乘法方式。设置为该值表示 将目标像素的颜色值设置为源像素的透明度值取反（即 1 - 源像素的透明度值）。
+
+	RenderTargetBlendDesc.BlendOp = D3D12_BLEND_OP_ADD;		// 表示混合操作的类型（加法、减法或反向减法），设置为该值表示 将源颜色和目标颜色相加。
+	RenderTargetBlendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;		// 表示源透明度和混合后透明度之间的乘法方式。设置为该值表示 将源像素的透明度值设置为 1。 F(1,1,1)
+	RenderTargetBlendDesc.DestBlendAlpha = D3D12_BLEND_ZERO;	// 表示目标透明度和混合后透明度之间的乘法方式。设置为该值表示 将源像素的透明度值设置为 0。F(0,0,0)
+	RenderTargetBlendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;	// 表示透明度混合操作的类型（加法、减法或反向减法）,设置为该值表示 将源颜色和目标颜色相加
+
+	RenderTargetBlendDesc.LogicOp = D3D12_LOGIC_OP_NOOP;		// 表示逻辑运算的类型（与、或、异或等）,设置为该值表示 不对输出颜色进行任何处理。
+	RenderTargetBlendDesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;	// 表示需要写入当前渲染目标的哪些颜色通道，设置为该值表示 启用所有通道。
+
+	// 设置渲染目标
+	DirectXPipelineState->SetRenderTarget(0, RenderTargetBlendDesc);
 
 	DirectXPipelineState->BuildPipelineState(EPipelineState::Selection);
 }
--- INVIEngine/Shader/HLSL/Select.hlsl
+++ INVIEngine/Shader/HLSL/Select.hlsl
@@ -5,13 +5,18 @@
 struct MeshVertexIn
 {
 	float3 Position : POSITION; // 位置
+	float4 Color : COLOR; // 颜色
+	float3 Normal : NORMAL; // 法线
+	float3 UTangent : TANGENT; // 切线（U方向）
 	float2 Texcoord : TEXCOORD; // UV
 };
 
 struct MeshVertexOut
 {
 	float4 PositionH : SV_POSITION;
+	float3 Normal : NORMAL;
 	float2 Texcoord : TEXCOORD;
+	float4 WorldPosition : POSITION;
 };
 
 MeshVertexOut VSMain(MeshVertexIn mv)
@@ -21,22 +26,25 @@ MeshVertexOut VSMain(MeshVertexIn mv)
 	MeshVertexOut Out = (MeshVertexOut) 0;
 	
 	// 将模型的顶点坐标转为世界坐标
-	float4 PositionWorld = mul(float4(mv.Position, 1.f), WorldMatrix);
+	Out.WorldPosition = mul(float4(mv.Position, 1.f), WorldMatrix);
 	
 	// 获取指向摄像机的向量
-	float3 ViewDir = normalize(CameraPosition.xyz - PositionWorld.xyz);
+	float3 ViewDir = normalize(CameraPosition.xyz - Out.WorldPosition.xyz);
 	
 	// 将模型的顶点坐标沿着摄像机指向的方向移动一点，这样就可以让射线选中的物体在摄像机前面从而被渲染出来
-	PositionWorld.xyz += ViewDir * 0.01f;
+	Out.WorldPosition.xyz += ViewDir * 0.01f;
 	
 	// 将世界坐标转为视口裁剪空间坐标
-	Out.PositionH = mul(PositionWorld, ViewportProjectionMatrix);
+	Out.PositionH = mul(Out.WorldPosition, ViewportProjectionMatrix);
 	
 	// UV
     // 先将传入的uv坐标和模型的纹理变换相乘，得到纹理变换后的UV
 	float4 TextureTexcoord = mul(float4(mv.Texcoord, 0.f, 1.f), TextureTransformationMatrix);
     // 然后再与材质的变换矩阵相乘，得到材质变换后的UV，就是我们最终的顶点UV了
 	Out.Texcoord = mul(TextureTexcoord, MatConstBuffer.MaterialProjectionMatrix).xy;
+	
+	// 法线
+	Out.Normal = mul(mv.Normal, (float3x3) WorldMatrix);
 	
 	return Out;
 
@@ -45,7 +53,11 @@ MeshVertexOut VSMain(MeshVertexIn mv)
 
 float4 PSMain(MeshVertexOut mvOut) : SV_TARGET
 {
-	return float4(0.9f, 0.23f, 0.11f, 1.0f);
+	float3 ModelNormal = normalize(mvOut.Normal);
+	float3 ViewDirection = normalize(CameraPosition.xyz - mvOut.WorldPosition.xyz);
+	float DotDiffValue = pow(1.f - max(dot(ModelNormal, ViewDirection), 0.0), 4.f);
+	
+	return float4(0.98f, 0.12f, 0.22f, 1.0f) * DotDiffValue;
 }
 
 
