97f6dae7c7003910d0d4d5a53814a741cc81f750
invi_1998 <2267661826@qq.com>
2024-01-08 18:10:14 +0800

    OperationHandle.HLSL

---
+++ INVIEngine/Shader/HLSL/OperationHandle.hlsl
@@ -0,0 +1,39 @@
+// 射线选中物体的shader
+
+#include "Material.hlsl"
+
+struct MeshVertexIn
+{
+	float3 Position : POSITION; // 位置
+};
+
+struct MeshVertexOut
+{
+	float4 PositionH : SV_POSITION;
+	float4 WorldPosition : POSITION;
+};
+
+MeshVertexOut VSMain(MeshVertexIn mv)
+{
+	
+	MeshVertexOut Out = (MeshVertexOut) 0;
+	
+	// 将模型的顶点坐标转为世界坐标
+	Out.WorldPosition = mul(float4(mv.Position, 1.f), WorldMatrix);
+	
+	// 将世界坐标转为视口裁剪空间坐标
+	Out.PositionH = mul(Out.WorldPosition, ViewportProjectionMatrix);
+	
+	return Out;
+
+}
+
+
+float4 PSMain(MeshVertexOut mvOut) : SV_TARGET
+{
+	MaterialConstBuffer MatConstBuffer = Materials[MaterialID];
+
+	return MatConstBuffer.BaseColor;
+}
+
+
