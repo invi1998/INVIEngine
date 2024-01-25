35a201bded683c7bede616915e693793acbce78d
invi_1998 <2267661826@qq.com>
2024-01-25 19:20:53 +0800

    缩放手柄

--- INVIEngine/Common/OperationHandleSelectManage.cpp
+++ INVIEngine/Common/OperationHandleSelectManage.cpp
@@ -1,2 +1,54 @@
 #include "EngineMinimal.h"
 #include "OperationHandleSelectManage.h"
+
+#include "Actor/Core/ActorObject.h"
+#include "SelectEditor/OperationHandle/MoveArrow.h"
+
+extern class GMoveArrow* MoveArrow;
+//extern class GRotateArrow* RotateArrow;
+//extern class GScaleArrow* ScaleArrow;
+extern class GActorObject* SelectedActor;
+
+FOperationHandleSelectManage::FOperationHandleSelectManage()
+{
+}
+
+FOperationHandleSelectManage::~FOperationHandleSelectManage()
+{
+}
+
+FOperationHandleSelectManage* FOperationHandleSelectManage::GetInstance()
+{
+}
+
+void FOperationHandleSelectManage::Destroy()
+{
+}
+
+void FOperationHandleSelectManage::AllOperationHandleHide()
+{
+}
+
+void FOperationHandleSelectManage::DisplaySelectedHandle(GActorObject* SelectedObject)
+{
+}
+
+void FOperationHandleSelectManage::DisplaySelectedHandle()
+{
+}
+
+void FOperationHandleSelectManage::HideSelectedHandle()
+{
+}
+
+void FOperationHandleSelectManage::SetNewSelectedOperationHandle(GActorObject* SelectedObject)
+{
+}
+
+void FOperationHandleSelectManage::SetNewSelectedSelectedObject()
+{
+}
+
+GActorObject* FOperationHandleSelectManage::GetSelectedOperationHandle()
+{
+}
--- INVIEngine/Common/OperationHandleSelectManage.h
+++ INVIEngine/Common/OperationHandleSelectManage.h
@@ -1,5 +1,32 @@
 #pragma once
-class OperationHandleSelectManage
+
+class GActorObject;
+
+class FOperationHandleSelectManage
 {
+public:
+	FOperationHandleSelectManage(void);
+	~FOperationHandleSelectManage(void);
+
+	static FOperationHandleSelectManage* GetInstance();
+	static void Destroy();
+
+	void AllOperationHandleHide();	//隐藏所有操作句柄
+
+	void DisplaySelectedHandle(GActorObject* SelectedObject);		//显示选择句柄
+	void DisplaySelectedHandle();		//显示选择句柄
+	void HideSelectedHandle();		//隐藏选择句柄
+
+	void SetNewSelectedOperationHandle(GActorObject* SelectedObject);		//显示新的操作句柄
+	void SetNewSelectedSelectedObject();		//设置新的选择对象
+
+	GActorObject* GetSelectedOperationHandle();		//获取选中的操作句柄
+
+private:
+	static FOperationHandleSelectManage* Instance;
+
+	GActorObject* SelectedOperationHandle;		//选中的操作句柄
+	GActorObject* SelectedObject;		//选中的对象
+
 };
 
