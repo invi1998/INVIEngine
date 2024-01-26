025d2ef87375bf705759b82f6cd51d468f6681ac
invi_1998 <2267661826@qq.com>
2024-01-26 15:40:55 +0800

    缩放操作句柄

---
+++ INVIEngine/Editor/SelectEditor/OperationHandle/ScalingArrow.cpp
@@ -0,0 +1,24 @@
+#include "EngineMinimal.h"
+#include "ScalingArrow.h"
+
+GScalingArrow::GScalingArrow()
+{
+}
+
+void GScalingArrow::SetMesh()
+{
+}
+
+void GScalingArrow::OnMouseMoved(int x, int y)
+{
+	GOperationHandle::OnMouseMoved(x, y);
+}
+
+void GScalingArrow::OnMouseLeftDown(int x, int y)
+{
+	GOperationHandle::OnMouseLeftDown(x, y);
+}
+
+void GScalingArrow::OnMousePressed(int x, int y)
+{
+}
---
+++ INVIEngine/Editor/SelectEditor/OperationHandle/ScalingArrow.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "OperationHandle.h"
+
+class GScalingArrow : public GOperationHandle
+{
+public:
+	GScalingArrow();
+
+	void SetMesh() override;
+
+	void OnMouseMoved(int x, int y) override;
+
+	void OnMouseLeftDown(int x, int y) override;
+
+	void OnMousePressed(int x, int y);
+};
+
